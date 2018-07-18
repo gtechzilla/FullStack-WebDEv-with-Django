ff913ed9e792b8b839773802f270e1b49aa995ca
gtechzilla <okiomagerald@gmail.com>
2018-07-18 21:39:14 +0300

    working app with templates,models & views

--- projects/django/first_project/first_app/views.py
+++ projects/django/first_project/first_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic,AccessRecord,Webpage
 # Create your views here.
 
 def index(request):
-    my_dict={'Insert_me':"This is what was inserted"}
-    return render(request,'first_app/index.html',context=my_dict)
+    webpages_list= AccessRecord.objects.order_by('date')
+    date_dict={'access_records':webpages_list}
+    return render(request,'first_app/index.html',context=date_dict)
