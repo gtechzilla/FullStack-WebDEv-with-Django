37dd93635d7d437c0c3fc242bfde6f6387be74c4
gtechzilla <okiomagerald@gmail.com>
2018-07-27 01:42:17 +0300

    validating all the form fields at once using the clean method

--- projects/django/Form_basics/form_basics/forms.py
+++ projects/django/Form_basics/form_basics/forms.py
@@ -3,19 +3,23 @@ from django import forms
 #django class that handles form validation
 from django.core import validators
 
-def check_for_gerald(value):
-    if value=='gerald':
-        raise forms.ValidationError('You cant use This name')
-
 class FormName(forms.Form):
-    name=forms.CharField(validators=[check_for_gerald])
+    name=forms.CharField()
     email=forms.EmailField()
+    verify_email=forms.EmailField(label='Retype your email')
     text=forms.CharField(widget=forms.Textarea)
 
-    #just add the validators attribute to the field u want to validate
-    botcatcher = forms.CharField(
-                                    required=False,
-                                    widget=forms.HiddenInput,
-                                    validators=[validators.MaxLengthValidator(0)]
-                                )
+    #clean method is used for cleaning form at once
+    def clean(self):
+
+        #super().clean grabs all the cleaned data from the form
+        all_cleaned_data = super().clean()
+
+        '''The clean method from supers store the cleaned data as a dictionary
+        to get a certain field you call the key of the dictionary'''
+        email=all_cleaned_data['email']
+        vmail= all_cleaned_data['verify_email']
+
+        if email !=vmail:
+            raise forms.ValidationError("Your Emails do not match")
 
