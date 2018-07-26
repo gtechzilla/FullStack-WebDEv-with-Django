42fbb313dd23378ddaaf683e3ebcc2137bbda50d
gtechzilla <okiomagerald@gmail.com>
2018-07-27 01:05:42 +0300

    intro to django inbuilt validators

--- projects/django/Form_basics/form_basics/forms.py
+++ projects/django/Form_basics/form_basics/forms.py
@@ -1,20 +1,13 @@
 from django import forms
+from django.core import validators
 
 class FormName(forms.Form):
     name=forms.CharField()
     email=forms.EmailField()
     text=forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False,widget=forms.HiddenInput)
+    botcatcher = forms.CharField(
+                                    required=False,
+                                    widget=forms.HiddenInput,
+                                    validators=[validators.MaxLengthValidator(0)]
+                                )
 
-    #function to check for the presence of bots filling out our forms
-    #syntax of the function is 'clean_"name of your field" '
-    def clean_botcatcher(self):
-
-        #uses the cleaned_data attribute of the Form class
-        botcatcher = self.cleaned_data['botcatcher']
-
-        #The condition to be flagged
-        if len(botcatcher) > 0:
-            raise forms.ValidationError("Found A Bot")
-
-        return botcatcher
