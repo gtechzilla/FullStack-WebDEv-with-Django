8ae2fb3c6afa8f41e01c1d93ff47815d626aa3ad
gtechzilla <okiomagerald@gmail.com>
2018-07-27 01:19:09 +0300

    creating ur own validations using django built in validators

--- projects/django/Form_basics/form_basics/forms.py
+++ projects/django/Form_basics/form_basics/forms.py
@@ -1,10 +1,18 @@
 from django import forms
+
+#django class that handles form validation
 from django.core import validators
 
+def check_for_gerald(value):
+    if value=='gerald':
+        raise forms.ValidationError('You cant use This name')
+
 class FormName(forms.Form):
-    name=forms.CharField()
+    name=forms.CharField(validators=[check_for_gerald])
     email=forms.EmailField()
     text=forms.CharField(widget=forms.Textarea)
+
+    #just add the validators attribute to the field u want to validate
     botcatcher = forms.CharField(
                                     required=False,
                                     widget=forms.HiddenInput,
