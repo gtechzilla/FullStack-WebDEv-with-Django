0ec14db5d381e9dc51a69175bb1efa1bd4df1aec
gtechzilla <okiomagerald@gmail.com>
2018-07-27 00:59:24 +0300

    manual validation when a bot fills your form page using a function call

--- projects/django/Form_basics/form_basics/forms.py
+++ projects/django/Form_basics/form_basics/forms.py
@@ -4,3 +4,17 @@ class FormName(forms.Form):
     name=forms.CharField()
     email=forms.EmailField()
     text=forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False,widget=forms.HiddenInput)
+
+    #function to check for the presence of bots filling out our forms
+    #syntax of the function is 'clean_"name of your field" '
+    def clean_botcatcher(self):
+
+        #uses the cleaned_data attribute of the Form class
+        botcatcher = self.cleaned_data['botcatcher']
+
+        #The condition to be flagged
+        if len(botcatcher) > 0:
+            raise forms.ValidationError("Found A Bot")
+
+        return botcatcher
